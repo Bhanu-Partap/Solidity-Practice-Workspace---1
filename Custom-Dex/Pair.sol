7b2c51c6efaa501cb00829e7f20506ad87be6357
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-01-10 16:31:04 +0530

    Creating Dex

--- Custom-Dex/Pair.sol
+++ Custom-Dex/Pair.sol
@@ -6,36 +6,47 @@ import "./Erc-20.sol";
 contract Pair {
 
     address owner;
+    uint pairId;
 
     struct pairdata{
-        address token0;
-        address token1;
+        erc20 token0;
+        erc20 token1;
+        string token0Name;
+        string token1Name;
         uint reserve0;
         uint reserve1;
-        uint Timestamp;    
-        uint id;
+        uint Timestamp; 
     }
+
+    mapping(uint256=>pairdata) public pairDetails;
+
     modifier  onlyOwner(){
     owner= msg.sender;
     _;
     }  
 
-    erc20 token0Address = new erc20(name_,symbol_,_totalSupply);
-    erc20 token1Address = new erc20(name_,symbol_,_totalSupply);
+    function createPair(string memory _token0Name, string memory _token0symbol, uint _token0totalSupply,string memory _token1Name, string memory _token1symbol, uint _token1totalSupply)public returns( pairdata memory ){      
+        erc20 token0Address= new erc20(_token0Name,_token0symbol,_token0totalSupply);
+        erc20 token1Address = new erc20(_token1Name,_token1symbol,_token1totalSupply);
+        pairId++;
+        pairDetails[pairId].token0= token0Address;
+        pairDetails[pairId].token1= token1Address;
+        pairDetails[pairId].token0Name= _token0Name;
+        pairDetails[pairId].token1Name= _token1Name;
+        pairDetails[pairId].token1Name= _token1Name;
 
-    mapping(uint=>pairdata) public tokenDetails;
 
-    function createPair(address token0, address token1)public returns(){
-        
+    
+        // return(pairDetails[pairId].token0Name);
     }
 
-    function depositLiquidity(uint _token0, uint _token1, address _to)public returns(uint){
+    // function depositLiquidity(uint _token0, uint _token1, address _to)public returns(uint){
 
-    }
+    // }
 
-    function withdrawLiquidity(uint _token0, uint _token1, address _to)public{
+    // function withdrawLiquidity(uint _token0, uint _token1, address _to)public{
 
-    }
+    // }
 
 
 
