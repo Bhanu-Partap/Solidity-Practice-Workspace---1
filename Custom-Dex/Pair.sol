166794461694a17d0dc2aaf2c6d5ac084b076ddb
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-01-10 14:18:44 +0530

    Creating Dex

--- Custom-Dex/Pair.sol
+++ Custom-Dex/Pair.sol
@@ -5,16 +5,39 @@ import "./Erc-20.sol";
 
 contract Pair {
 
-uint fee;
+    address owner;
 
-struct pairdata{
-    address token0;
-    address token1;
-    uint reserve0;
-    uint reserve1;
-    uint blockTimestampLast;    
-    uint id;
-}
-mapping(uint=>pairdata) public tokenDetails;
+    struct pairdata{
+        address token0;
+        address token1;
+        uint reserve0;
+        uint reserve1;
+        uint Timestamp;    
+        uint id;
+    }
+    modifier  onlyOwner(){
+    owner= msg.sender;
+    _;
+    }  
+
+    erc20 token0Address = new erc20(name_,symbol_,_totalSupply);
+    erc20 token1Address = new erc20(name_,symbol_,_totalSupply);
+
+    mapping(uint=>pairdata) public tokenDetails;
+
+    function createPair(address token0, address token1)public returns(){
+        
+    }
+
+    function depositLiquidity(uint _token0, uint _token1, address _to)public returns(uint){
+
+    }
+
+    function withdrawLiquidity(uint _token0, uint _token1, address _to)public{
+
+    }
+
+
+
 
 }
