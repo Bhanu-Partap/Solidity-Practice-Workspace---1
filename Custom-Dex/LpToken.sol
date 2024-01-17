a236cec98b74f082e000a4061fe44aadae888b5c
Bhanu-Partap <bhanupartap25275@gmail.com>
2024-01-17 13:52:44 +0530

    Creating Custom Dex

--- Custom-Dex/LpToken.sol
+++ Custom-Dex/LpToken.sol
@@ -3,6 +3,21 @@ import "./Erc-20.sol";
 contract LpTokens{
 
     erc20 token0;
+    erc20 token1;
+    erc20 lptokens;
 
-    function deposit
+
+    constructor(address _token0, address _token1) {
+        token0 = erc20(_token0);
+        token1 = erc20(_token1);
+    }
+
+    function mintingLpTokens(uint _amount0, uint _amount1) external {
+        require(_amount0 > 0 && _amount1 > 0, "Amounts must be greater than zero");
+        uint256 totalSupply = totalSupply(); 
+        require(token0.transferFrom(msg.sender, address(this), _amount0), "TokenA transfer failed");
+        require(token1.transferFrom(msg.sender, address(this), _amount1), "TokenB transfer failed");
+        erc20.mint(msg.sender,address(this),_amount0 + _amount1);
+    }
 }
+
